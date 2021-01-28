aa0dba919039273967f92ea4bfed15007eae6a6a
Rubina <rubina_lakdawala@yahoo.com>
2021-01-28 19:21:33 +0530

    gsc with total counts

--- src/app/routes/gscdata/gscdata/gscdata.component.ts
+++ src/app/routes/gscdata/gscdata/gscdata.component.ts
@@ -7,6 +7,7 @@ import { Label } from 'ng2-charts';
 import { DatePipe } from '@angular/common';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { parseDate } from 'ngx-bootstrap/chronos';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-gscdata',
   templateUrl: './gscdata.component.html',
@@ -20,7 +21,7 @@ export class GscdataComponent implements OnInit {
   previousStartDate;
   previousEndDate;
   toDate = new FormControl(new Date())
-  fromDate = new FormControl(new Date(Date.now() - 7 * 24 * 60 * 60 * 1000) )
+  fromDate = new FormControl(new Date(Date.now() - 7 * 24 * 60 * 60 * 1000))
   currYear: number;
   myForm = new FormGroup({
     fromDate: this.fromDate,
@@ -28,6 +29,18 @@ export class GscdataComponent implements OnInit {
   });
   startDate;
   endDate;
+  clicksThisYear: string = "0";
+  clicksPreviousYear: string = "0";
+  impressionsThisYear: string = "0";
+  impressionsPreviousYear: string = "0";
+  cTRThisYear: string = "0";
+  cTRPreviousYear: string = "0";
+  positionThisYear: string = "0";
+  positionPreviousYear: string = "0";
+  percentClicks;
+  percentImpressions;
+  percentCTR;
+  percentPosition;
   //Chart variables
   barOptions = {
     scaleShowVerticalLines: false,
@@ -98,7 +111,9 @@ export class GscdataComponent implements OnInit {
         label: function (tooltipItems, data) {
           //var idx = tooltipItems.index;
           //return data.labels[idx] + ' €';
-          return tooltipItems.yLabel + '  Keywords'
+          return tooltipItems.yLabel
+
+          //return tooltipItems.yLabel + '  Keywords'
         }
       }
     },
@@ -180,13 +195,13 @@ export class GscdataComponent implements OnInit {
       'Authorization': 'Bearer ' + this.accessToken,
     })
   };
-  constructor(private authService: SocialAuthService, private http: HttpClient, public datepipe: DatePipe,) {
+  constructor(private authService: SocialAuthService, private http: HttpClient, public datepipe: DatePipe, public router: Router) {
     this.getDateSettings();
   }
   signInWithGoogle(): void {
     const googleLoginOptions = {
       scope: 'profile email https://www.googleapis.com/auth/webmasters.readonly https://www.googleapis.com/auth/webmasters'
-    }; 
+    };
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID, googleLoginOptions)
       .then((res) => {
         this.accessToken = res['authToken'];
@@ -195,8 +210,8 @@ export class GscdataComponent implements OnInit {
       })
   }
 
-  getDataCurrentYear(startDate, endDate) {
-    debugger
+  getDataCurrentYear(startDate, endDate, all) {
+    
     this.httpOptionJSON = {
       headers: new HttpHeaders({
         'Accept': 'application/json',
@@ -204,43 +219,62 @@ export class GscdataComponent implements OnInit {
       })
     };
     const url = "https://www.googleapis.com/webmasters/v3/sites/https%3A%2F%2Fwww.abhisi.com%2F/searchAnalytics/query?key=AIzaSyC1IsrCeeNXp9ksAmC8szBtYVjTLJC9UWQ";
-    let data = {
-      "startRow": 0,
-      "startDate": startDate,
-      "endDate": endDate,
-      "dataState": "ALL",
-      "dimensions": [
-        "DATE"
-      ]
-    };
+    let data = {};
+    if (all == 1) {
+      data = {
+        "startRow": 0,
+        "startDate": startDate,
+        "endDate": endDate,
+        "dataState": "ALL",
+        "dimensions": [
+          "DATE"
+        ]
+      };
+    }
+    if (all == 0) {
+      data = {
+        "startRow": 0,
+        "startDate": startDate,
+        "endDate": endDate,
+        "dataState": "ALL",
+      };
+    }
     this.http.post(url, data, this.httpOptionJSON).subscribe(res => {
       if (res) {
-        debugger
+        
         let rows = res['rows'];
-        this.dateListData = [];
-        this.barDataClicks.datasets[0].data = [];
-        this.barDataImpressions.datasets[0].data = [];
-        this.barDataCTR.datasets[0].data = [];
-        this.barDataPositions.datasets[0].data = [];
-        this.barChartLabels = [];
-        for (let i = 0; i < rows.length; i++) {
-          this.currDate = rows[i].keys[0]
-          this.currDate = this.currDate.substring(5, 10);
-          this.barDataClicks.datasets[0].data.push(rows[i].clicks);
-          this.barDataImpressions.datasets[0].data.push(rows[i].impressions);
-          this.barDataCTR.datasets[0].data.push(rows[i].ctr);
-          this.barDataPositions.datasets[0].data.push(rows[i].position);
-          this.barChartLabels.push(this.currDate);
+        if (all == 0) {
+          this.clicksThisYear = rows[0].clicks;
+          this.impressionsThisYear = rows[0].impressions;
+          this.cTRThisYear =  parseFloat(rows[0].ctr).toFixed(2).toString();
+          this.positionThisYear = parseFloat(rows[0].position).toFixed(2).toString();
+        }
+        else {
+          this.dateListData = [];
+          this.barDataClicks.datasets[0].data = [];
+          this.barDataImpressions.datasets[0].data = [];
+          this.barDataCTR.datasets[0].data = [];
+          this.barDataPositions.datasets[0].data = [];
+          this.barChartLabels = [];
+          for (let i = 0; i < rows.length; i++) {
+            this.currDate = rows[i].keys[0]
+            this.currDate = this.currDate.substring(5, 10);
+            this.barDataClicks.datasets[0].data.push(rows[i].clicks);
+            this.barDataImpressions.datasets[0].data.push(rows[i].impressions);
+            this.barDataCTR.datasets[0].data.push(rows[i].ctr);
+            this.barDataPositions.datasets[0].data.push(rows[i].position);
+            this.barChartLabels.push(this.currDate);
+          }
         }
       }
     }, error => {
-      debugger
+      
       alert('Data fetch failed for current year : ' + JSON.stringify(error.error));
     });
 
   }
-  getDataPreviousYear(startDate, endDate) {
-    debugger
+  getDataPreviousYear(startDate, endDate, all) {
+    
     this.httpOptionJSON = {
       headers: new HttpHeaders({
         'Accept': 'application/json',
@@ -259,25 +293,37 @@ export class GscdataComponent implements OnInit {
     };
     this.http.post(url, data, this.httpOptionJSON).subscribe(res => {
       if (res) {
-        debugger
+        
         let rows = res['rows'];
-        this.dateListData = [];
-        this.barDataClicks.datasets[1].data = [];
-        this.barDataImpressions.datasets[1].data = [];
-        this.barChartLabels = [];
-        for (let i = 0; i < rows.length; i++) {
-          this.currDate = rows[i].keys[0]
-          this.currDate = this.currDate.substring(5, 10);
-          this.barDataClicks.datasets[1].data.push(rows[i].clicks + 10);
-          this.barDataImpressions.datasets[1].data.push(rows[i].impressions);
-          this.barDataCTR.datasets[1].data.push(rows[i].ctr);
-          this.barDataPositions.datasets[1].data.push(rows[i].position);
+        if (all == 0) {
+          this.clicksPreviousYear = rows[0].clicks;
+          this.impressionsPreviousYear =  rows[0].impressions;
+          this.cTRPreviousYear = parseFloat(rows[0].ctr).toFixed(2).toString();
+          this.positionPreviousYear  = parseFloat(rows[0].position).toFixed(2).toString();
 
-          this.barChartLabels.push(this.currDate);
+          //pecentgage calculate
+          //(previous-current*100)/Previous
+          
+        }
+        else {
+          this.dateListData = [];
+          this.barDataClicks.datasets[1].data = [];
+          this.barDataImpressions.datasets[1].data = [];
+          this.barChartLabels = [];
+          for (let i = 0; i < rows.length; i++) {
+            this.currDate = rows[i].keys[0]
+            this.currDate = this.currDate.substring(5, 10);
+            this.barDataClicks.datasets[1].data.push(rows[i].clicks + 10);
+            this.barDataImpressions.datasets[1].data.push(rows[i].impressions);
+            this.barDataCTR.datasets[1].data.push(rows[i].ctr);
+            this.barDataPositions.datasets[1].data.push(rows[i].position);
+
+            this.barChartLabels.push(this.currDate);
+          }
         }
       }
     }, error => {
-      debugger
+      
       alert('Data fetch failed for previous year : ' + JSON.stringify(error.error));
     });
 
@@ -300,27 +346,44 @@ export class GscdataComponent implements OnInit {
     this.previousEndDate = prevYear.toString() + '-' + this.datepipe.transform(this.toDate.value, 'MM-dd');
     this.getData();
   }
-  getData(){
-    debugger
-    if(this.accessToken == '' || this.accessToken == undefined || this.accessToken ==null){
+  getData() {
+    
+    if (this.accessToken == '' || this.accessToken == undefined || this.accessToken == null) {
       alert("Please, Login with Google to fetch data");
-    }else if(parseDate(this.endDate) < parseDate(this.startDate)){
+    } else if (parseDate(this.endDate) < parseDate(this.startDate)) {
       alert("Start Date can not be grater then End Date");
     }
-    else{
-    this.getDataCurrentYear(this.startDate, this.endDate);
-    this.getDataPreviousYear(this.previousStartDate, this.previousEndDate);
+    else {
+      this.getDataCurrentYear(this.startDate, this.endDate, 0);
+      this.getDataPreviousYear(this.previousStartDate, this.previousEndDate, 0);
+      this.getDataCurrentYear(this.startDate, this.endDate, 1);
+      this.getDataPreviousYear(this.previousStartDate, this.previousEndDate, 1);
     }
   }
-  getDateSettings(){
-    debugger
+  getDateSettings() {
+    
     this.startDate = this.datepipe.transform(this.fromDate.value, 'yyyy-MM-dd');
     this.currYear = parseInt(this.datepipe.transform(this.fromDate.value, 'yyyy'));
     let prevYear = this.currYear - 1;
     this.previousStartDate = prevYear.toString() + '-' + this.datepipe.transform(this.fromDate.value, 'MM-dd');
     this.endDate = this.datepipe.transform(this.toDate.value, 'yyyy-MM-dd');
     this.currYear = parseInt(this.datepipe.transform(this.toDate.value, 'yyyy'));
-     prevYear = this.currYear - 1;
+    prevYear = this.currYear - 1;
     this.previousEndDate = prevYear.toString() + '-' + this.datepipe.transform(this.toDate.value, 'MM-dd');
+  }
+  public goToLinkedIn(event) {
+
+    this.router.navigate([`/linkedin`])
+  }
+  public goToInstagram(event) {
+
+    this.router.navigate([`/instagram`])
+  }
+  public goToGSC(event) {
+
+    this.router.navigate([`/gsc`])
+  }
+  getPercentage(num, total) {
+    return ((100 * num) / total)
   }
 }
