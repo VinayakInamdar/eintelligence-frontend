2e71439d362721a0f434b1e1b62eb17926497fbe
Techovarya <Techovarya@DESKTOP-6084LTU>
2021-08-18 13:09:36 +0530

    confirmm

--- src/app/routes/linkedin/linkedin/linkedin.component.ts
+++ src/app/routes/linkedin/linkedin/linkedin.component.ts
@@ -15,16 +15,64 @@ export class LinkedinComponent implements OnInit {
     redirectUrl: "https://localhost:4200/linkedin/linkedin"
   };
   //constructor(private _linkedInService: LinkedInService) { }
-  constructor(private http: HttpClient,  private activatedRoute: ActivatedRoute) { 
-    
-    this.accessToken = this.activatedRoute.snapshot.queryParams["code"];
-  }
+ 
 
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  // accessToken;
+  // private linkedInCredentials = {
+  //   clientId: "77rl4v6oym4y16",
+  //   redirectUrl: "https://localhost:4200/linkedin/linkedin"
+  // };
+  // settingActive;
+  //constructor(private _linkedInService: LinkedInService) { }
+  //   constructor(private http: HttpClient,  private activatedRoute: ActivatedRoute) {
+  // debugger
+  //     this.accessToken = this.activatedRoute.snapshot.queryParams["code"];
+  //   }
+  linkedInToken;
   public isUserAuthenticated;
   apiKey;
   ngOnInit() {
+    if (localStorage.getItem("isLinkedinUserAuthenticated")) {
+      debugger
+      this.linkedInToken = this.route.snapshot.queryParams["code"];
+      localStorage.removeItem("isLinkedinUserAuthenticated");
+    }
 
   }
+  // login() {
+  //   window.location.href = `https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=${
+  //     this.linkedInCredentials.clientId
+  //   }&redirect_uri=${this.linkedInCredentials.redirectUrl}&scope=r_emailaddress`;
+  // }
+  // connectToLinkedIn() {
+  //   let url = `https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=${
+  //     this.linkedInCredentials.clientId
+  //   }&redirect_uri=${this.linkedInCredentials.redirectUrl}&scope=r_emailaddress`;
+  //   this.http.get(url).subscribe(res => {
+  //     if (res) {
+
+  //     }
+  //   }, error => {
+
+  //     alert(error.message);
+  //   });
+  // let url = "https://www.linkedin.com/oauth/v2/accessToken";
+  // let body = {
+  // 	grant_type:"authorization_code",
+  // 	code:"AQQkyz3W6F58reu6QnRnXhi2l5mA6CevvQaArjhwEgjgFWE9elVAu-eesvajzCM7wrfgYKl4X_tKGkenct44x9EZii6x2646G3xbQP62K_gqPWjhj5NqkqtfAW_rIMEppAIrYnom0lwyKD8Lfs3T_fKukyWllR1GK_wNJmHrjnZbH4HR8Gjm8I9ibcZNeQ",
+  // 	redirect_uri:"http://localhost:4200/component/linkedin/callback",
+  // 	client_id:"866ucec32xxjp9",
+  // 	client_secret:"s2bKnc6Zjet6M9ZB"
+  // }
+  // this.http.post(url, body).subscribe(res => {
+  // 	if (res) {
+  // 	}
+  // }, error => {
+  // 	alert(error.message);
+  // });
+
+  //}
   login() {
     window.location.href = `https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=${
       this.linkedInCredentials.clientId
@@ -57,5 +105,6 @@ export class LinkedinComponent implements OnInit {
     // 	alert(error.message);
     // });
 
-    }
+    
   }
+}
